4de7d75fb392e6ed71a7b0aed656a35120a9a72e
FCraven <craven.francis@gmail.com>
2019-02-02 15:27:51 -0500

    installed redux, imported createStore to index.js,  crfeted Store and reducer, handed reducer to createStore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux'
+
 const balance = document.getElementById("balance");
 const deposit5 = document.getElementById("deposit5");
 const deposit25 = document.getElementById("deposit25");
@@ -8,3 +10,14 @@ deposit5.onclick = () => console.log("Deposit 5");
 deposit25.onclick = () => console.log("Deposit 25");
 withdraw5.onclick = () => console.log("Withdraw 5");
 withdraw25.onclick = () => console.log("Withdraw 25");
+
+
+const reducer = (state = {balance: 0}, action) => {
+  return state
+}
+
+const store= createStore(reducer)
+
+store.subscribe(() => {
+  console.log('The store state changed. Here is the new State:', store.getState())
+})
